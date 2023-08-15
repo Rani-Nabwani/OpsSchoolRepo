6d619fb2ebbf15be073a4308552f49b12b09a8d7
Rani-Nabwani <nivuani.r@gmail.com>
2023-08-15 14:31:45 +0300

    Add script to fix network configuration

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-#add fix to exercise1 here
+
+CONFIG_FILE="/etc/netplan/50-vagrant.yaml"
+
+
+sed -i 's/dhcp4: /dhcp4: true/' $CONFIG_FILE
+
+
+sudo netplan apply $CONFIG_FILE
+
+echo "Network configuration updated and applied successfully."
